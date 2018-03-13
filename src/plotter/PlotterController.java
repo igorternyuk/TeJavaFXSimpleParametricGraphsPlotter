f8ee132fbf1c2b19bbd9ec1d6e4724b6a64b58bf
igorternyuk <mr.ternyuk@mail.ru>
2018-03-13 18:44:21 +0200

    fixes bug with spinner

--- src/plotter/PlotterController.java
+++ src/plotter/PlotterController.java
@@ -2,21 +2,35 @@ package plotter;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.stage.FileChooser;
 import parser.Expression;
 import parser.MathParser;
 
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class PlotterController implements Initializable {
     private static final String EXIT_DIALOG_WINDOW_MESSAGE = "Do you really want to exit?";
     private static final String EXIT_DIALOG_WINDOW_TITLE = "Confirm exit, please";
+    private static final String SNAPSHOT_SAVING_ERROR_TEXT = "Could not save snapshot";
+    private static final String DEFAULT_SNAPSHOT_NAME = "snapshot.png";
+    private static final int LINE_THICKNESS_MAX = 100;
+    private static final int DEFAULT_LINE_WIDTH = 2;
+    private static final double SPINNER_STEP = 0.01;
+    private static final double DEFAULT_PARAMETER_STEP = 0.01;
+
     @FXML
     private Canvas canvas;
 
@@ -70,17 +84,19 @@ public class PlotterController implements Initializable {
     public void initialize(){
         parser = new MathParser();
         gc = canvas.getGraphicsContext2D();
-        spinnerLineThickness.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 100,
-                2, 1));
+        spinnerLineThickness.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, LINE_THICKNESS_MAX,
+                DEFAULT_LINE_WIDTH, 1));
         spinnerTmin.setEditable(true);
         spinnerTmin.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(Double.MIN_VALUE,
-                Double.MAX_VALUE, 0.0, 0.01));
+                Double.MAX_VALUE, 0, SPINNER_STEP));
         spinnerTmax.setEditable(true);
         spinnerTmax.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(Double.MIN_VALUE,
-                Double.MAX_VALUE, 0.0, 0.01));
+                Double.MAX_VALUE, 0, SPINNER_STEP));
         spinnerDeltaT.setEditable(true);
-        spinnerDeltaT.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(Double.MIN_VALUE,
-                Double.MAX_VALUE, 0.01, 0.01));
+        SpinnerValueFactory<Double> factory = new SpinnerValueFactory.DoubleSpinnerValueFactory(Double.MIN_VALUE,
+                Double.MAX_VALUE, DEFAULT_PARAMETER_STEP, SPINNER_STEP);
+        spinnerDeltaT.setValueFactory(factory);
+
         timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
@@ -93,8 +109,6 @@ public class PlotterController implements Initializable {
                 draw();
             }
         };
-
-        System.out.println("Initialize");
     }
 
     private void draw() {
@@ -102,18 +116,6 @@ public class PlotterController implements Initializable {
             gc.strokeLine(prevPoint.getX(), prevPoint.getY(), currPoint.getX(), currPoint.getY());
         }
     }
-
-    /*
-    * private void showErrorMessage(String msg){
-        System.out.println(msg);
-        Alert alert = new Alert(Alert.AlertType.ERROR, msg, ButtonType.OK);
-        alert.setTitle("Error");
-        alert.showAndWait();
-        if (alert.getResult() == ButtonType.OK) {
-            alert.close();
-        }
-    }
-    * */
 
     private Point2D curveFunction(){
         final double y = funcY.evaluate();
@@ -131,10 +133,10 @@ public class PlotterController implements Initializable {
         try {
             prevPoint = null;
             currPoint = null;
-            tmin = Double.parseDouble(spinnerTmin.getEditor().getText());
+            tmin = spinnerTmin.getValue();
             t = tmin;
-            tmax = Double.parseDouble(spinnerTmax.getEditor().getText());
-            dt = Double.parseDouble(spinnerDeltaT.getEditor().getText());
+            tmax = spinnerTmax.getValue();
+            dt = spinnerDeltaT.getValue()/*.doubleValue()*/;
             parser.addVariable("t", tmin);
             funcX = parser.parse(txtFunctionX.getText());
             funcY = parser.parse(txtFunctionY.getText());
@@ -142,9 +144,9 @@ public class PlotterController implements Initializable {
             gc.setLineWidth(spinnerLineThickness.getValue());
             startTimer();
         } catch (Exception ex){
-
+            showErrorMessage(ex.getMessage());
+            stopTimer();
         }
-        startTimer();
     }
 
     public void onStartStopAnimationButtonClicked(){
@@ -165,23 +167,31 @@ public class PlotterController implements Initializable {
         isAnimationActive = true;
     }
 
-    public void onSaveGraphAsImageButtonClicked(){
-        /*
-        * try{
-            FileChooser fileChooser = new FileChooser();
-            FileChooser.ExtensionFilter filter = new FileChooser
-                    .ExtensionFilter("Select a file (*.png)", "*.png");
-            fileChooser.getExtensionFilters().add(filter);
-            File file = fileChooser.showSaveDialog(null);
+    private void showErrorMessage(final String message) {
+        final Alert alert = new Alert(Alert.AlertType.ERROR, message, ButtonType.OK);
+        alert.setTitle(SNAPSHOT_SAVING_ERROR_TEXT);
+        alert.showAndWait();
+        if (alert.getResult() == ButtonType.OK) alert.close();
+    }
 
-            Image snapshot = canvasBottom.snapshot(null, null);
-            ImageIO.write(SwingFXUtils.fromFXImage(snapshot, null),
-                    "png", file != null ? file : new File("snapshot.png"));
-        }
-        catch(IOException ex){
+    public void onSaveGraphAsImageButtonClicked(){
+        try {
+            final FileChooser fileChooser = new FileChooser();
+            final FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Select a file (*.png)", "*.png");
+            fileChooser.getExtensionFilters().add(filter);
+            final File file = fileChooser.showSaveDialog(null);
+            final Image snapshot = canvas.snapshot(null, null);
+            ImageIO.write(SwingFXUtils.fromFXImage(snapshot, null), "png",
+                    file != null ? file : new File(DEFAULT_SNAPSHOT_NAME));
+        } catch (IOException ex) {
             showErrorMessage(ex.toString());
         }
-        * */
+    }
+
+    public void onClearCanvasClicked(ActionEvent actionEvent) {
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        txtFunctionX.clear();
+        txtFunctionY.clear();
     }
 
     public void onCloseAppButtonClicked(){
